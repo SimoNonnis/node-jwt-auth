8be180676a67b4fb6e24e0925998a312d39709b5
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-09 11:48:58 +0100

    :art: Add jwt and cors

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 1234;
+const users = [
+  { id: 1, username: 'admin', password: 'admin' },
+  { id: 2, username: 'guest', password: 'guest' }
+];
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/status', (req, res) => {
   const localTime = new Date().toLocaleTimeString();
@@ -15,9 +23,30 @@ app.get('*', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  const username = req.body.username;
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send('You need a username and password');
+    return;
+  }
 
-  res.status(200).send(`Username: ${username}`);
+  const user = users.find(
+    u => u.username === req.body.username && u.password === req.body.password
+  );
+
+  if (!user) {
+    res.status(401).send('User not found!');
+    return;
+  }
+
+  const token = jwt.sign(
+    {
+      sub: user.id,
+      username: user.username
+    },
+    'mysupersecretkey',
+    { expiresIn: '1 hour' }
+  );
+
+  res.status(200).send({ access_token: token });
 });
 
 app.listen(PORT, () => console.log(`App listening at ${PORT} port`));
