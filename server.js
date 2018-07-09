be314697c2e59dae8e1c6392b11e620d9001a8cd
Simon Nonnis <simon.nonnis@protonmail.com>
2018-07-09 10:53:52 +0100

    :art: Add body-parser

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = 1234;
+const PORT = process.env.PORT || 1234;
+
+app.use(bodyParser.json());
 
 app.get('/status', (req, res) => {
   const localTime = new Date().toLocaleTimeString();
@@ -9,6 +12,12 @@ app.get('/status', (req, res) => {
 
 app.get('*', (req, res) => {
   res.sendStatus(404);
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+
+  res.status(200).send(`Username: ${username}`);
 });
 
 app.listen(PORT, () => console.log(`App listening at ${PORT} port`));
